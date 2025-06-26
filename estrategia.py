e828797cb7dd6cfbba7fac52c677f86c10993fde
Roberto Alemán <alemanbarahona@gmail.com>
2025-06-26 12:43:09 -0600

    Update estrategia.py

--- estrategia.py
+++ estrategia.py
@@ -7,11 +7,16 @@ def simular_sesion(df, payout, stake_pct, martingala, ciclos_max, tp_pct, sl_pct
     saldo_max = saldo
     saldo_min = saldo
     entradas_idx = []  # ← nuevo: guardar posiciones reales de entrada
+    entradas_filtradas_idx = []
+
 
     for i in range(2, len(df)):
         if df['color'][i-2] == df['color'][i-1] and df['color'][i] != df['color'][i-1]:
             if usar_rsi and not (40 <= df['RSI'][i] <= 60):
+                entradas_filtradas_por_rsi += 1
+                entradas_filtradas_idx.append(i)  # Guardamos el índice de esta oportunidad ignorada
                 continue
+
 
             entradas += 1
             entradas_idx.append(i)  # ← nuevo: guardar punto de entrada real
@@ -45,5 +50,6 @@ def simular_sesion(df, payout, stake_pct, martingala, ciclos_max, tp_pct, sl_pct
         'saldo_final': saldo,
         'drawdown_max': (saldo_max - saldo_min)/saldo_max * 100,
         'historial': historial,
-        'entradas_idx': entradas_idx  # ← nuevo: devuelve los índices
+        'entradas_idx': entradas_idx,  # ← nuevo: devuelve los índices
+        'entradas_filtradas_idx': entradas_filtradas_idx
     }
